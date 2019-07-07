b88586247bfeee46cc283798ae8a519904bf3944
Christian Awelakoue <awelakoue@gmail.com>
2019-07-07 04:44:28 +0200

    structure && add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyC-e_92XMA7JnPFJ5ooWvMp-mL_dGegP3E",
+    authDomain: "ng-blog-b5836.firebaseapp.com",
+    databaseURL: "https://ng-blog-b5836.firebaseio.com",
+    projectId: "ng-blog-b5836",
+    storageBucket: "ng-blog-b5836.appspot.com",
+    messagingSenderId: "636493010415",
+    appId: "1:636493010415:web:b1abd550d05a0422"
+  }
 };
 
 /*
